f19b8488a77dfaf96097c1e28f123b9ebfb87edd
Daniel Cairoli <daniel.cairoli@icloud.com>
2026-02-18 16:55:51 +0100

    change model to to 26s

--- live-cam.py
+++ live-cam.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo26x.pt")
+#model = YOLO("yolo26s.pt")
+model = YOLO("yolo26s-pose.pt")
+#model = YOLO("yolo26x.pt")
 #model = YOLO("yolo26x-pose.pt")
 
 
