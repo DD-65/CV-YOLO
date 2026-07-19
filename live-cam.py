d071d4adc766f8a57b050344a2ca2715c56d998a
Daniel Cairoli <daniel.cairoli@icloud.com>
2026-07-19 13:03:39 +0200

    various models

--- live-cam.py
+++ live-cam.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-#model = YOLO("yolo26s.pt")
-model = YOLO("yolo26s-pose.pt")
+model = YOLO("yolo26s.pt")
+#model = YOLO("yolo26s-pose.pt")
 #model = YOLO("yolo26x.pt")
 #model = YOLO("yolo26x-pose.pt")
 
